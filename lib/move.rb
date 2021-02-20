59d71255e1703680cafe6da34cf3d1db38d75698
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-02-20 19:50:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,7 +14,7 @@ def input_to_index(index)
 index.to_i-1
 end
 
-def move(move)
-  update_array_at_with(array, index, value)
-    array[index] = value
+def move(board, index, player="X")
+  #update_array_at_with(array, index, value)
+    board[index] = player
 end
